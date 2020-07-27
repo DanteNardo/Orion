907d95a66ecf87708e9d0d876b3e912ec2d06fbc
Dantenardo <dante.nardo@outlook.com>
2020-07-26 18:40:06 -0700

    Initial Commit

---
+++ Orion/Assets/Scripts/Force.cs
@@ -0,0 +1,45 @@
+﻿
+
+/*******************************************/
+/*                  INCLUDES               */
+/*******************************************/
+using UnityEngine;
+
+/*******************************************/
+/*                   CLASS                 */
+/*******************************************/
+public class Force {
+
+    /***************************************/
+    /*               MEMBERS               */
+    /***************************************/
+    private Vector3 force;
+    public bool force_enabled = true;
+
+    /***************************************/
+    /*              PROPERTIES             */
+    /***************************************/
+
+
+    /***************************************/
+    /*               METHODS               */
+    /***************************************/
+    public Force(float force) {
+        this.force = new Vector3(force, force, force);
+	}
+
+    public Force(Vector3 force) {
+        this.force = force;
+	}
+
+    public Vector3 Get() {
+        if (force_enabled) {
+            return force;
+        }
+        else return Vector3.zero;
+	}
+
+    public void Update(Vector3 force) {
+        this.force = force;
+	}
+}
---
+++ Orion/Assets/Scripts/KinematicController.cs
@@ -0,0 +1,51 @@
+﻿
+
+/*******************************************/
+/*                  INCLUDES               */
+/*******************************************/
+using System.Collections.Generic;
+using UnityEngine;
+
+/*******************************************/
+/*                   CLASS                 */
+/*******************************************/
+[RequireComponent(typeof(PhysicsObject))]
+public class KinematicController : MonoBehaviour {
+
+	/***************************************/
+	/*               MEMBERS               */
+	/***************************************/
+
+	// Constants 
+	const float GRAVITY = -9.81f;
+
+	// Components
+	private PhysicsObject physics_object;
+
+	/***************************************/
+	/*              PROPERTIES             */
+	/***************************************/
+	private List<Force> Forces { get; set; } = new List<Force>();
+	public Force Gravity = new Force(new Vector3(0, GRAVITY, 0));
+	public Force Friction = new Force(new Vector3(0, 0, 0));
+	public Force Drag = new Force(new Vector3(0, 0, 0));
+
+	/***************************************/
+	/*               METHODS               */
+	/***************************************/
+	private void Start() {
+		physics_object = GetComponent<PhysicsObject>();
+		Forces.Add(Gravity);
+		Forces.Add(Friction);
+	}
+
+	private void FixedUpdate() {
+		foreach (var force in Forces) {
+			physics_object.AddForce(force);
+		}
+	}
+
+	/***************************************/
+	/*              COROUTINES             */
+	/***************************************/
+}
